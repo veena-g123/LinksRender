38ebdf22b42ce41d6468834a6b325ffc50a59c8b
veena-g123 <57742830+veena-g123@users.noreply.github.com>
2024-09-28 13:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,31 @@ function loadVideos(file) {
     fetch(`https://raw.githubusercontent.com/veena-g123/LinksRender/refs/heads/main/data/${file}.txt`)
         .then(response => response.text())
         .then(data => {
-            const links = data.trim().split('\n');
+            const links = data.split('\n');
             const container = document.getElementById('videoContainer');
             container.innerHTML = '';
-            console.log(links.length)
             links.forEach(link => {
+                const col = document.createElement('div');
+                col.className = 'col-12 mb-4';
                 const iframe = document.createElement('iframe');
                 iframe.src = getEmbedLink(link);
-                iframe.width = '200';
-                iframe.height = '150';
-                container.appendChild(iframe);
+                iframe.width = '100%';
+                iframe.height = '315';
+                iframe.className = 'lazyload';
+                col.appendChild(iframe);
+                container.appendChild(col);
             });
         });
 }
 
 function getEmbedLink(link) {
-    // if (link.includes('instagram.com')) {
-    //     return `https://www.instagram.com/p/${link.split('/p/')[1].split('/')[0]}/embed`;
-    // }
     if (link.includes('instagram.com/p/')) {
         return `https://www.instagram.com/p/${link.split('/p/')[1].split('/')[0]}/embed`;
     } else if (link.includes('instagram.com/reel/')) {
         return `https://www.instagram.com/reel/${link.split('/reel/')[1].split('/')[0]}/embed`;
-    }else if (link.includes('instagram.com/reels/')) {
+    } else if (link.includes('instagram.com/reels/')) {
         return `https://www.instagram.com/reels/${link.split('/reels/')[1].split('/')[0]}/embed`;
     }
     return link;
 }
+
