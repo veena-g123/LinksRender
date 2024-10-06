2df7029be1d34de9a447ad6a1c97a01dc25a2708
veena-g123 <57742830+veena-g123@users.noreply.github.com>
2024-10-06 10:40:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+const map = {
+    "java": "https://docs.google.com/document/d/e/2PACX-1vRYuu-6wlqbrVVWUxDqCQ500Zqxq9crxOFqgfTsVb_16h6GcCJzvgyaBCwAnBrylJnsvSMQM1iD4Js7/pub",
+    "spring-boot": 'https://docs.google.com/document/d/e/2PACX-1vSsAMCvxNfXv3lFLOIxnjNhG9zvJ55q2xScnaWbqdRewRn0dBfvPe0Ra1_xgBPXw9h16G-M-FLcyclS/pub',
+    "mysql": "https://docs.google.com/document/d/e/2PACX-1vShi1FRWKkGpy9lQBCKbkL5vyIk5NIj5nHXwPRR4cJelG2cULxRpsk4aP8ePDUNSgik-2ENgGefSZ6-/pub",
+    'hr-discussion': "https://docs.google.com/document/d/e/2PACX-1vS-atrq4pC2fPlXvFBnCCbO4t5sOyePFPFCarRqhYS5h9YnZzwQVE-s677x94JDB2JzI7SK0MPbsFgE/pub",
+    'junit': "https://docs.google.com/document/d/e/2PACX-1vSncT-FUCmSl3IkUdqeY3FHQQwr-HFJMYyxbxoqNisTNIF-lXncO8Bmm7Q0DhoLXJVX_wBAt8wh190f/pub",
+    'mockito': "https://docs.google.com/document/d/e/2PACX-1vTzVHAmLy2xI1VZgPyx3ow3WJFFz_7vcVT__TqY1bejQdebUCnEq9tFeVBjymKAFAjkjikstcGJw3aP/pub",
+    'javaScript': "https://docs.google.com/document/d/e/2PACX-1vRdZA9o6whOcc3mi9HnRGpYfs9WBArYTLN84a_SLUnc04nkGigeP2ituhp8LCuIWBEgcOA_x_f_wqJj/pub",
+    'misc': "https://docs.google.com/document/d/e/2PACX-1vRLRF-1BeTDIkGNq4b5HDG2o3cZUUN8klgrjpRGQwRzehKkT-BQnMKW_JXKEH0lBJsxpYkk6piZ-i2u/pub"
+}
 function loadVideos(file) {
-    fetch(`https://raw.githubusercontent.com/geetsswain/study-resources-links/refs/heads/main/${file}.txt`)
-        .then(response => response.text())
+    console.log(map[file])
+    const base = 'https://api.allorigins.win/get?url=' + encodeURIComponent(map[file]);
+    fetch(base)
+        .then(response => response.json())
         .then(data => {
-            const links = data.split('\n');
+            console.log(data)
+            let content = '';
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data.contents, 'text/html');
+            const paragraphs = doc.body.getElementsByTagName('p');
+            for (let p of paragraphs) {
+                content += p.innerText + '\n';
+            }
+
+            const links = content.split('\n');
             const container = document.getElementById('videoContainer');
             container.innerHTML = '';
             links.forEach(link => {
+                console.log(link);
+                link = link.trim();
                 const col = document.createElement('div');
                 col.className = 'col-12 mb-4';
                 const iframe = document.createElement('iframe');
@@ -19,6 +42,7 @@ function loadVideos(file) {
         });
 }
 
+
 function getEmbedLink(link) {
     if (link.includes('instagram.com/p/')) {
         return `https://www.instagram.com/p/${link.split('/p/')[1].split('/')[0]}/embed`;
@@ -30,3 +54,29 @@ function getEmbedLink(link) {
     return link;
 }
 
+// function loadVideos() {
+//     const url = 'https://api.allorigins.win/get?url=' + encodeURIComponent('https://docs.google.com/document/d/e/2PACX-1vSsAMCvxNfXv3lFLOIxnjNhG9zvJ55q2xScnaWbqdRewRn0dBfvPe0Ra1_xgBPXw9h16G-M-FLcyclS/pub');
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             const parser = new DOMParser();
+//             const doc = parser.parseFromString(data.contents, 'text/html');
+//             const paragraphs = doc.body.getElementsByTagName('p');
+
+//             let content = '';
+//             for (let p of paragraphs) {
+//                 content += p.innerText + '\n';
+//             }
+//             console.log(content)
+//             // const container = document.getElementById('videoContainer');
+//             // container.innerHTML = '';
+//             // links.forEach(link => {
+//             //     console.log(link.href);
+//             //     const linkElement = document.createElement('a');
+//             //     linkElement.href = link.href;
+//             //     linkElement.textContent = link.href;
+//             //     container.appendChild(linkElement);
+//             // });
+//         });
+// }
